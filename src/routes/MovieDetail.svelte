525076234c4964121abecffc79a140f676dee80c
Ivan-Trubin <ew4n.trubin@yandex.ru>
2024-03-21 00:42:49 +0700

    SvelteKit

---
+++ src/routes/MovieDetail.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export async function load({ params }) {
+      // Получаем информацию о фильме из параметров маршрута
+      const { title } = params;
+      // Ваш код для загрузки информации о фильме по его названию
+      const movie = { title: 'Фильм', description: 'Описание фильма' }; // Заглушка данных
+      return { movie };
+    }
+  </script>
+  
+  <script>
+    // Принимаем свойство movie, содержащее информацию о выбранном фильме
+    export let movie = {};
+  
+    // Добавим функцию для обработки нажатия на ссылку назад
+    function goBack() {
+      history.back();
+    }
+  </script>
+  
+  <h2>{movie.title}</h2>
+  <p>{movie.description}</p>
+  
+  <button on:click={goBack}>Назад</button>
---
+++ src/routes/MoviesList.svelte
@@ -0,0 +1,17 @@
+<script>
+    // Заглушка данных о фильмах (ваш список фильмов)
+    let movies = [
+      { title: 'Фильм 1', description: 'Описание фильма 1' },
+      { title: 'Фильм 2', description: 'Описание фильма 2' },
+      { title: 'Фильм 3', description: 'Описание фильма 3' },
+      // Добавьте больше фильмов по вашему усмотрению
+    ];
+  </script>
+  
+  <h2>Список фильмов:</h2>
+  
+  <ul>
+    {#each movies as movie}
+      <li><a href="/movies/{encodeURIComponent(movie.title)}">{movie.title}</a></li>
+    {/each}
+  </ul>
